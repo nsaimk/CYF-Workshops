b966d760f08ebcbdd826891af54bc45a792c70ff
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-04-16 13:16:13 +0100

    Debugging -2, 5

--- debugging/2.js
+++ debugging/2.js
@@ -3,6 +3,13 @@ This function should return the value of the largest number in the given array.
 Can you fix the bug in this function
 
 */
+function sum(arr){
+  let res = 0;
+  for (let i = 0; i < arr.length; i++){
+    res += arr[i]
+  }
+  return res
+}
 
 function getLargestNum(arr) {
     let largestNum = 0;
@@ -16,3 +23,4 @@ function getLargestNum(arr) {
   let arr = [1, 2, 3, 4, 5];
 
   getLargestNum(arr)
+  sum(arr)
--- debugging/5.js
+++ debugging/5.js
@@ -4,14 +4,16 @@ This code contains more than one error. Can you find and fix the errors.
 
 let list1 = [1, 2, 3, 4, 5];
 let list2 = [6, 7, 8, 9, 10];
+let to = list1 + list2
+console.log(typeof to)
 
 function mergeLists(list1, list2) {
   let mergedList = [];
   for (let i = 0; i < list1.length; i++) {
-    mergedList.push(list1[i]);
-    for (let j = 0; j < list2.length; i++) {
+    mergedList.push(list1[i], list2[i]);
+/*     for (let j = 0; j < list2.length; j++) {
       mergedList.push(list2[j]);
-    }
+    } */
   }
   return mergedList;
 }
